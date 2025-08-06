9dbe1549bfe20486fa3005ba2c0c3e75c83bdc44
Poseico <jorgelherreralobo@gmail.com>
2025-08-06 16:19:10 -0500

    Añadir funcionalidad para eliminar tareas y actualizar README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ToDoApp-Group2
+Aplicación de lista de tareas de Python.
+
+## Instalacion
+1. Clona el repositorio.
+2. Instala Python 3.X.
+3. Ejecuta 'python main.py'.
+
+## Funcinalidades
+- Crear tareas.
+- Eliminar tareas.
--- Task_model.py
+++ Task_model.py
@@ -5,3 +5,10 @@ class TaskModel:
 
     def get_task_name(self):
         return self.task_name
+    
+    def delete_task_name(self):
+        self.task_name = None
+        self.is_completed = False
+
+    def is_completed(self):
+        return self.is_completed
